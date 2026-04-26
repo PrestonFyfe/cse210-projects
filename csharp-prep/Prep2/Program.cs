263a0ec831e626e1901df57333f51616dda50140
PrestonFyfe <prestoncharlesfyfe@gmail.com>
2026-04-25 22:06:25 -0600

    Created letter grade program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.Write("What is your current grade percentage? ");
+        string grade_in_text = Console.ReadLine();
+        float grade_percent = float.Parse(grade_in_text);
+
+        if (grade_percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade_percent >= 80) 
+        {
+            letter = "B";
+        }
+        else if (grade_percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade_percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }  
+        Console.WriteLine($"You got an {letter}");
+
+        if (grade_percent >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass :(");
+        }
     }
 }
