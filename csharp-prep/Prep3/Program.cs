872901e094a35481f918c06828afe1110794e295
PrestonFyfe <prestoncharlesfyfe@gmail.com>
2026-04-30 22:37:24 -0600

    Completed Prep #3 Guess My Number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int total_guesses = 1;
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 11);
+        //Console.Write("What is the magic number? ");
+        //int answer = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != answer)
+        {
+            if (guess > answer)
+            {
+                Console.WriteLine("Lower");
+            } else
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            total_guesses += 1;
+        }
+
+        Console.WriteLine($"You guessed it in {total_guesses} guesses!");
     }
 }
