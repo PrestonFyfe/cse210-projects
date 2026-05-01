13a83bd2b89437b2a32c19fcd3dacebb0417e1e0
PrestonFyfe <prestoncharlesfyfe@gmail.com>
2026-04-30 23:01:57 -0600

    Completed Prep #4 number list and data types.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int response = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (response != 0)
+        {
+            Console.Write("Enter number: ");
+            response = int.Parse(Console.ReadLine());
+            if (response != 0)
+            {
+                numbers.Add(response);
+            }
+        }
+        
+        int sum = 0;
+        int largest_num = numbers[0];
+        float average = 0;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > largest_num)
+            {
+                largest_num = num;
+            }
+        }
+        average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {largest_num}");
     }
 }
